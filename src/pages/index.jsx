c43d83b32d7f2412f0e9696aa24e15d73f06c96e
Trey White <treywhite21@gmail.com>
2023-05-18 16:42:54 -0700

    Enable resume download

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -223,8 +223,14 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
+      <Button
+        href="trey-white_resume_june-2021.pdf"
+        target="_blank"
+        download
+        variant="secondary"
+        className="group mt-6 w-full"
+      >
+        Download Resume (2021)
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
