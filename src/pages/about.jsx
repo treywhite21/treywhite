f3c186241e997d0ee52f739da6e859b16fa28b7c
Trey White <treywhite21@gmail.com>
2023-05-23 16:20:36 -0700

    Update About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,12 +6,10 @@ import clsx from 'clsx'
 import { Container } from '@/components/Container'
 import {
   GitHubIcon,
-  InstagramIcon,
   LinkedInIcon,
-  TwitterIcon,
   SubstackIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/photos/image-about.jpeg'
 
 function SocialLink({ className, href, children, icon: Icon, size }) {
   return (
@@ -50,7 +48,7 @@ export default function About() {
         <title>About - Trey White</title>
         <meta
           name="description"
-          content="I’m Trey White. I live in San Diego, where I try to build cool stuff that makes peoples' lives easier."
+          content="I'm Trey White. I live in San Diego, where I try to build cool stuff that makes peoples' lives easier."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -72,30 +70,93 @@ export default function About() {
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I've loved making things for as long as I can remember. I think
+                most people my age spent many of their formative years attending
+                the LEGO school of construction.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                As I grew out of childhood and playing with blocks, I fell in
+                love with technology. Honestly, it's a lot like construction
+                except my supplies are infinite and my crane, hammer, etc. is a
+                keyboard on a laptop that I can take with me anywhere.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                I was lucky enough to attend a high school with a speciality
+                center for information technology where I fell deeper into
+                technology's grip as I started programming. I continued this
+                throughout my college years at Virginia Tech.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                After college, I went to Washington, DC to work for Deloitte
+                Consulting where I had a nice variation in types of projects and
+                experiences. The nature of consulting was great in terms of
+                exposure to different organizations and styles of work, but
+                tended to be slower than I prefered, filled with governmental
+                red tape, and higher ups trying harder to sell work than to
+                solve problems.
+              </p>
+              <p>
+                So when my girlfriend at the time, now wife, decided to travel
+                nurse. I saw it as an opportunity to make a 180.
+              </p>
+              <p>
+                I had been attending real estate investment masterminds with my
+                dad for a few years at this point and, it was obvious to me
+                that, there was a need for software to save these investors.
+                They had no idea where to begin with organizing, tracking, and
+                managing their investment portfolios.
+              </p>
+              <p>
+                This seemed like an opportunity to help people with the skills I
+                had and none of the slow-pace and red tape of my job at the
+                time. My girlfiend and I soon left for Nashville, TN where she
+                would start her journey as a travel nurse and I would create my
+                company, RealNumberz, with my dad as the idea guy and myself as
+                the tech nerd.
+              </p>
+              <p>
+                Starting a new company with absolutely zero-guidance is a
+                daunting but exciting experience. I loved designing and building
+                the product itself, but my original assumption that users would
+                come running to me waving money in my face and asking to pay
+                me... turned out wasn't <em>exactly</em> correct.
+              </p>
+              <p>
+                To address this, I dove into learning all things business and we
+                made changes to the marketing and sales aspects. People began
+                using and paying for the app. It was an amazing feeling, but
+                great feelings don't pay the rent, and working in a team of one
+                gets lonely.
+              </p>
+              <p>
+                As my girlfriend's nursing contract in Nashville was expiring
+                she signed a new contract in Seattle, WA. I decided to interview
+                with some big tech companies. I knew what I wanted to do at this
+                point, and I wanted to do it with the smartest people to learn
+                from them and build better products.
+              </p>
+              <p>
+                I began working for Microsoft which was great. Even through
+                COVID and riots in Seattle, I loved my life (for a multitude of
+                reasons). I did this for two years until my manager wanted me to
+                come back into the office. This was at the same time that my
+                girlfriend had started a new nursing contract in San Diego, CA.
+              </p>
+              <p>
+                We had fallen in love with San Diego. Our dog had fallen in love
+                with the beach. I couldn't go back.
+              </p>
+              <p>
+                I left Microsoft and began working for Google. I've been there
+                ever since. It's been an amazing experience, and I'm happier
+                than I've ever been. I'll talk your ear off about it and annoy
+                the heck out of you, if you want me to.
+              </p>
+              <p>
+                Oh, and the cherry on top: my dad and I rebranded the company I
+                started in Nashville from RealNumberz to REI Copilot, and it's
+                now running itself with the user base growing and helping more
+                people than ever.
               </p>
             </div>
           </div>
